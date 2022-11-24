507ebd8b642ffbc254d2bb7a03359487f0b15f1a
Spoorti <spoortidaddi5@gmail.com>
2022-11-24 15:17:00 +0530

    AppConstatnt modified

--- src/main/java/com/java/hospital/constants/AppConstant.java
+++ src/main/java/com/java/hospital/constants/AppConstant.java
@@ -5,6 +5,6 @@ public interface AppConstant {
 	public static String AdminController="/AdminController";
 	public static String PatientController="/PatientController";
 	public static String SavePatientDetails="/savePatientDetails";
-	public static String Save="";
+	
 	
 }
